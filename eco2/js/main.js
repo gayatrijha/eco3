758d4b28042cf205a4dd99b1d0a73a750b364ad7
gayatrijha <jha.gayatri01@gmail.com>
2021-08-25 16:19:37 +0545

    first commit

---
+++ eco2/js/main.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel();
+  });
+
+  $(window).on('load', function() {
+    $('#exampleModal').modal('show');
+});
+ $(".owl-carousel").owlCarousel({
+    loop:true,
+    items:1,
+   });
+function myFunction() {
+  var input, filter, ul, li, a, i;
+  input = document.getElementById("mySearch");
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("myMenu");
+  li = ul.getElementsByTagName("li");
+  for (i = 0; i < li.length; i++) {
+    a = li[i].getElementsByTagName("a")[0];
+    if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
+    }
+  }
+}
+
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
